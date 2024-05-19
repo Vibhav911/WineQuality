0ebf6b093bba15be27ae551d0660cfc20660056e
Vibhav Ucharia <vibhav09112003@gmail.com>
2024-05-19 16:46:40 +0530

    Model Trainer added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from WineQProject import logger
 from WineQProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from WineQProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from WineQProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from WineQProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 
 # Data Ingestion Stage
@@ -35,6 +36,18 @@ try:
     obj = DataTransformationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} finished <<<<<< \n\n x==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+# Model Trainer
+STAGE_NAME = "Model Trainer Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/WineQProject/config/configuration.py
+++ src/WineQProject/config/configuration.py
@@ -3,7 +3,8 @@ from WineQProject.constants import *
 from WineQProject.utils.common import read_yaml, create_directories
 from WineQProject.entity.config_entity import (DataIngestionConfig,
                                                DataValidationConfig,
-                                               DataTransformationConfig)
+                                               DataTransformationConfig,
+                                               ModelTrainerConfig)
 
 
 # This program reads yaml file, creates directories and stores the paths from constants & config/config file
@@ -66,3 +67,25 @@ class ConfigurationManager:
         )
         
         return data_transformation_config
+    
+    
+    
+    # Model Trainer
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema = self.schema.TARGET_COLUMN
+        
+        create_directories([config.root_dir])
+        
+        model_trainer_config = ModelTrainerConfig(
+            root_dir = config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name
+        )
+        
+        return model_trainer_config
